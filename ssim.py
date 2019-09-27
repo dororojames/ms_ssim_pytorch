60afa431d2d4b6d755366717d32f675467c411e9
Dororo <james7751129@gamil.com>
2019-09-28 01:03:04 +0800

    miner update

--- ssim.py
+++ ssim.py
@@ -1,11 +1,18 @@
 '''
-code modified from
-https://github.com/VainF/pytorch-msssim/blob/master/pytorch_msssim/ssim.py
+© 2019, JamesChan
+forked from https://github.com/One-sixth/ms_ssim_pytorch/ssim.py
 '''
-
 import torch
 import torch.jit
 import torch.nn.functional as F
+from torch import arange, exp, mean, prod, stack, sum
+
+
+@torch.jit.script
+def rerange(tensor):
+    if torch.max(tensor) > 1.5:
+        return tensor
+    return torch.round(tensor * 255.)
 
 
 @torch.jit.script
@@ -17,14 +24,13 @@ def create_window(window_size: int, sigma: float, channel: int):
     :param channel: input channel
     :return: 1D kernel
     '''
-    coords = torch.arange(window_size, dtype=torch.float)
+    coords = arange(window_size, dtype=torch.float)
     coords -= window_size // 2
 
-    g = torch.exp(-(coords ** 2) / (2 * sigma ** 2))
+    g = exp(-(coords ** 2) / (2 * sigma ** 2))
     g /= g.sum()
 
-    g = g.reshape(1, 1, 1, -1).repeat(channel, 1, 1, 1)
-    return g
+    return g.reshape(1, 1, 1, -1).repeat(channel, 1, 1, 1)
 
 
 @torch.jit.script
@@ -36,18 +42,16 @@ def _gaussian_filter(x, window_1d, use_padding: bool):
     :param use_padding: padding image before conv
     :return: blured tensors
     '''
-    C = x.shape[1]
-    padding = 0
-    if use_padding:
-        window_size = window_1d.shape[3]
-        padding = window_size // 2
+    C = x.size(1)
+    padding = 0 if not use_padding else window_1d.size(3) // 2
     out = F.conv2d(x, window_1d, stride=1, padding=(0, padding), groups=C)
-    out = F.conv2d(out, window_1d.transpose(2, 3), stride=1, padding=(padding, 0), groups=C)
+    out = F.conv2d(out, window_1d.transpose(2, 3),
+                   stride=1, padding=(padding, 0), groups=C)
     return out
 
 
 @torch.jit.script
-def ssim(X, Y, window, data_range: float, use_padding: bool=False):
+def ssim(X, Y, window, data_range: float, use_padding: bool = False):
     '''
     Calculate ssim index for X and Y
     :param X: images
@@ -58,8 +62,7 @@ def ssim(X, Y, window, data_range: float, use_padding: bool=False):
     :return:
     '''
 
-    K1 = 0.01
-    K2 = 0.03
+    K1, K2 = 0.01, 0.03
     compensation = 1.0
 
     C1 = (K1 * data_range) ** 2
@@ -89,7 +92,7 @@ def ssim(X, Y, window, data_range: float, use_padding: bool=False):
 
 
 @torch.jit.script
-def ms_ssim(X, Y, window, data_range: float, weights, use_padding: bool=False):
+def ms_ssim(X, Y, window, data_range: float, weights, use_padding: bool = False):
     '''
     interface of ms-ssim
     :param X: a batch of images, (N,C,H,W)
@@ -100,64 +103,77 @@ def ms_ssim(X, Y, window, data_range: float, weights, use_padding: bool=False):
     :param use_padding: padding image before conv
     :return:
     '''
-    levels = weights.shape[0]
-    cs_vals = []
-    ssim_vals = []
+    levels = weights.size(0)
+    cs_vals, ssim_vals = [], []
     for _ in range(levels):
-        ssim_val, cs = ssim(X, Y, window=window, data_range=data_range, use_padding=use_padding)
+        ssim_val, cs = ssim(X, Y, window=window,
+                            data_range=data_range, use_padding=use_padding)
         cs_vals.append(cs)
         ssim_vals.append(ssim_val)
-        padding = (X.shape[2] % 2, X.shape[3] % 2)
-        X = F.avg_pool2d(X, kernel_size=2, stride=2, padding=padding)
-        Y = F.avg_pool2d(Y, kernel_size=2, stride=2, padding=padding)
+        padding = (X.size(-2) % 2, X.size(-1) % 2)
+        X = F.avg_pool2d(X, kernel_size=2, padding=padding)
+        Y = F.avg_pool2d(Y, kernel_size=2, padding=padding)
 
-    cs_vals = torch.stack(cs_vals, dim=0)
-    ms_ssim_val = torch.prod((cs_vals[:-1] ** weights[:-1].unsqueeze(1)) * (ssim_vals[-1] ** weights[-1]), dim=0)
+    ms_cs_vals = stack(cs_vals[:-1], dim=0) ** weights[:-1].unsqueeze(1)
+    ms_ssim_val = prod(ms_cs_vals * (ssim_vals[-1] ** weights[-1]), dim=0)
     return ms_ssim_val
 
 
 class SSIM(torch.jit.ScriptModule):
-    __constants__ = ['data_range', 'use_padding']
+    __constants__ = ['scale', 'data_range', 'use_padding', 'reduction']
 
-    def __init__(self, window_size=11, window_sigma=1.5, data_range=255., channel=3, use_padding=False):
+    def __init__(self, window_size=11, window_sigma=1.5, scale=True, data_range=255., channel=3, use_padding=False, reduction="none"):
         '''
         :param window_size: the size of gauss kernel
         :param window_sigma: sigma of normal distribution
+        :param scale: rerange to 255.
         :param data_range: value range of input images. (usually 1.0 or 255)
         :param channel: input channels (default: 3)
         :param use_padding: padding image before conv
+        :param reduction: reduction mode
         '''
         super().__init__()
         assert window_size % 2 == 1, 'Window size must be odd.'
         window = create_window(window_size, window_sigma, channel)
         self.register_buffer('window', window)
-        self.data_range = data_range
+        self.scale = scale
+        self.data_range = 255. if scale else data_range
         self.use_padding = use_padding
+        self.reduction = reduction
 
     @torch.jit.script_method
-    def forward(self, X, Y):
-        r = ssim(X, Y, window=self.window, data_range=self.data_range, use_padding=self.use_padding)
-        return r[0]
+    def forward(self, input, target):
+        if self.scale:
+            input, target = rerange(input), rerange(target)
+
+        ret = ssim(input, target, window=self.window,
+                   data_range=self.data_range, use_padding=self.use_padding)[0]
+        if self.reduction != 'none':
+            ret = mean(ret) if self.reduction == 'mean' else sum(ret)
+        return ret
 
 
 class MS_SSIM(torch.jit.ScriptModule):
-    __constants__ = ['data_range', 'use_padding']
+    __constants__ = ['scale', 'data_range', 'use_padding', 'reduction']
 
-    def __init__(self, window_size=11, window_sigma=1.5, data_range=255., channel=3, use_padding=False, weights=None, levels=None):
+    def __init__(self, window_size=11, window_sigma=1.5, scale=True, data_range=255., channel=3, use_padding=False, weights=None, levels=None, reduction="none"):
         '''
-        class for ms-ssim
         :param window_size: the size of gauss kernel
         :param window_sigma: sigma of normal distribution
+        :param scale: rerange to 255.
         :param data_range: value range of input images. (usually 1.0 or 255)
         :param channel: input channels
         :param use_padding: padding image before conv
         :param weights: weights for different levels. (default [0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
         :param levels: number of downsampling
+        :param reduction: reduction mode
         '''
         super().__init__()
         assert window_size % 2 == 1, 'Window size must be odd.'
-        self.data_range = data_range
+        self.scale = scale
+        self.data_range = 255. if scale else data_range
         self.use_padding = use_padding
+        self.reduction = reduction
 
         window = create_window(window_size, window_sigma, channel)
         self.register_buffer('window', window)
@@ -173,180 +189,12 @@ class MS_SSIM(torch.jit.ScriptModule):
         self.register_buffer('weights', weights)
 
     @torch.jit.script_method
-    def forward(self, X, Y):
-        return ms_ssim(X, Y, window=self.window, data_range=self.data_range, weights=self.weights,
-                       use_padding=self.use_padding)
+    def forward(self, input, target):
+        if self.scale:
+            input, target = rerange(input), rerange(target)
 
-
-if __name__ == '__main__':
-    print('Simple Test')
-    im = torch.randint(0, 255, (5, 3, 256, 256), dtype=torch.float, device='cuda')
-    img1 = im / 255
-    img2 = img1 * 0.5
-
-    losser = SSIM(data_range=1.).cuda()
-    loss = losser(img1, img2).mean()
-
-    losser2 = MS_SSIM(data_range=1.).cuda()
-    loss2 = losser2(img1, img2).mean()
-
-    print(loss.item())
-    print(loss2.item())
-
-
-if __name__ == '__main__':
-    print('Training Test')
-    import cv2
-    import torch.optim
-    import numpy as np
-    import imageio
-    import time
-
-    out_test_video = False
-    # 最好不要直接输出gif图，会非常大，最好先输出mkv文件后用ffmpeg转换到GIF
-    video_use_gif = False
-
-    im = cv2.imread('test_img1.jpg', 1)
-    t_im = torch.from_numpy(im).cuda().permute(2, 0, 1).float()[None] / 255.
-
-    if out_test_video:
-        if video_use_gif:
-            fps = 0.5
-            out_wh = (im.shape[1]//2, im.shape[0]//2)
-            suffix = '.gif'
-        else:
-            fps = 5
-            out_wh = (im.shape[1], im.shape[0])
-            suffix = '.mkv'
-        video_last_time = time.perf_counter()
-        video = imageio.get_writer('ssim_test'+suffix, fps=fps)
-
-    # 测试ssim
-    print('Training SSIM')
-    rand_im = torch.randint_like(t_im, 0, 255, dtype=torch.float32) / 255.
-    rand_im.requires_grad = True
-    optim = torch.optim.Adam([rand_im], 0.003, eps=1e-8)
-    losser = SSIM(data_range=1., channel=t_im.shape[1]).cuda()
-    ssim_score = 0
-    while ssim_score < 0.999:
-        optim.zero_grad()
-        loss = losser(rand_im, t_im)
-        (-loss).sum().backward()
-        ssim_score = loss.item()
-        optim.step()
-        r_im = np.transpose(rand_im.detach().cpu().numpy().clip(0, 1) * 255, [0, 2, 3, 1]).astype(np.uint8)[0]
-        r_im = cv2.putText(r_im, 'ssim %f' % ssim_score, (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-
-        if out_test_video:
-            if time.perf_counter() - video_last_time > 1. / fps:
-                video_last_time = time.perf_counter()
-                out_frame = cv2.cvtColor(r_im, cv2.COLOR_BGR2RGB)
-                out_frame = cv2.resize(out_frame, out_wh, interpolation=cv2.INTER_AREA)
-                if isinstance(out_frame, cv2.UMat):
-                    out_frame = out_frame.get()
-                video.append_data(out_frame)
-
-        cv2.imshow('ssim', r_im)
-        cv2.setWindowTitle('ssim', 'ssim %f' % ssim_score)
-        cv2.waitKey(1)
-
-    if out_test_video:
-        video.close()
-
-    # 测试ms_ssim
-    if out_test_video:
-        if video_use_gif:
-            fps = 0.5
-            out_wh = (im.shape[1]//2, im.shape[0]//2)
-            suffix = '.gif'
-        else:
-            fps = 5
-            out_wh = (im.shape[1], im.shape[0])
-            suffix = '.mkv'
-        video_last_time = time.perf_counter()
-        video = imageio.get_writer('ms_ssim_test'+suffix, fps=fps)
-
-    print('Training MS_SSIM')
-    rand_im = torch.randint_like(t_im, 0, 255, dtype=torch.float32) / 255.
-    rand_im.requires_grad = True
-    optim = torch.optim.Adam([rand_im], 0.003, eps=1e-8)
-    losser = MS_SSIM(data_range=1., channel=t_im.shape[1]).cuda()
-    ssim_score = 0
-    while ssim_score < 0.999:
-        optim.zero_grad()
-        loss = losser(rand_im, t_im)
-        (-loss).sum().backward()
-        ssim_score = loss.item()
-        optim.step()
-        r_im = np.transpose(rand_im.detach().cpu().numpy().clip(0, 1) * 255, [0, 2, 3, 1]).astype(np.uint8)[0]
-        r_im = cv2.putText(r_im, 'ms_ssim %f' % ssim_score, (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-
-        if out_test_video:
-            if time.perf_counter() - video_last_time > 1. / fps:
-                video_last_time = time.perf_counter()
-                out_frame = cv2.cvtColor(r_im, cv2.COLOR_BGR2RGB)
-                out_frame = cv2.resize(out_frame, out_wh, interpolation=cv2.INTER_AREA)
-                if isinstance(out_frame, cv2.UMat):
-                    out_frame = out_frame.get()
-                video.append_data(out_frame)
-
-        cv2.imshow('ms_ssim', r_im)
-        cv2.setWindowTitle('ms_ssim', 'ms_ssim %f' % ssim_score)
-        cv2.waitKey(1)
-
-    if out_test_video:
-        video.close()
-
-
-if __name__ == '__main__':
-    print('Performance Testing SSIM')
-    import time
-    s = SSIM(data_range=1.).cuda()
-
-    a = torch.randint(0, 255, size=(20, 3, 256, 256), dtype=torch.float32).cuda() / 255.
-    b = a * 0.5
-    a.requires_grad = True
-    b.requires_grad = True
-
-    start_record = torch.cuda.Event(enable_timing=True)
-    end_record = torch.cuda.Event(enable_timing=True)
-
-    start_time = time.perf_counter()
-    start_record.record()
-    for _ in range(500):
-        loss = s(a, b).mean()
-        loss.backward()
-    end_record.record()
-    end_time = time.perf_counter()
-
-    torch.cuda.synchronize()
-
-    print('cuda time', start_record.elapsed_time(end_record))
-    print('perf_counter time', end_time-start_time)
-
-
-if __name__ == '__main__':
-    print('Performance Testing MS_SSIM')
-    import time
-    s = MS_SSIM(data_range=1.).cuda()
-
-    a = torch.randint(0, 255, size=(20, 3, 256, 256), dtype=torch.float32).cuda() / 255.
-    b = a * 0.5
-    a.requires_grad = True
-    b.requires_grad = True
-
-    start_record = torch.cuda.Event(enable_timing=True)
-    end_record = torch.cuda.Event(enable_timing=True)
-
-    start_time = time.perf_counter()
-    start_record.record()
-    for _ in range(500):
-        loss = s(a, b).mean()
-        loss.backward()
-    end_record.record()
-    end_time = time.perf_counter()
-
-    torch.cuda.synchronize()
-
-    print('cuda time', start_record.elapsed_time(end_record))
-    print('perf_counter time', end_time-start_time)
+        ret = ms_ssim(input, target, window=self.window, data_range=self.data_range,
+                      weights=self.weights, use_padding=self.use_padding)
+        if self.reduction != 'none':
+            ret = mean(ret) if self.reduction == 'mean' else sum(ret)
+        return ret
