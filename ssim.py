0301f4bbdbbb2cf447df8362a2cc55c24ed0c175
Dororo <james7751129@gamil.com>
2019-10-08 20:23:42 +0800

    miner update

--- ssim.py
+++ ssim.py
@@ -3,69 +3,63 @@
 forked from https://github.com/One-sixth/ms_ssim_pytorch/ssim.py
 '''
 import torch
-import torch.jit
 import torch.nn.functional as F
-from torch import arange, exp, mean, prod, stack, sum
+from torch import arange, prod, stack
 
 
 @torch.jit.script
 def create_window(window_size: int, sigma: float, channel: int):
-    '''
-    Create 1-D gauss kernel
-    :param window_size: the size of gauss kernel
-    :param sigma: sigma of normal distribution
-    :param channel: input channel
-    :return: 1D kernel
-    '''
+    """create 1D gauss kernel  
+    Args:
+        window_size (int): the size of gauss kernel
+        sigma (float): sigma of normal distribution
+        channel (int): input channel
+    """
     half_window = window_size // 2
-    coords = arange(-half_window, half_window+1, dtype=torch.float)
+    coords = arange(-half_window, half_window+1).float()
 
-    g = exp(-(coords ** 2) / (2 * sigma ** 2))
-    g /= g.sum()
+    g = (-(coords ** 2) / (2 * sigma ** 2)).exp_()
+    g.div_(g.sum())
 
     return g.reshape(1, 1, 1, -1).repeat(channel, 1, 1, 1)
 
 
 @torch.jit.script
-def _gaussian_filter(x, window_1d, use_padding: bool):
-    '''
-    Blur input with 1-D kernel
-    :param x: batch of tensors to be blured
-    :param window_1d: 1-D gauss kernel
-    :param use_padding: padding image before conv
-    :return: blured tensors
-    '''
+def gaussian_blur(x, window, use_padding: bool):
+    """Blur input with 1-D gauss kernel  
+    Args:
+        x (tensor): batch of tensors to be blured
+        window (tensor): 1-D gauss kernel
+        use_padding (bool): padding image before conv
+    """
     C = x.size(1)
-    padding = 0 if not use_padding else window_1d.size(3) // 2
-    out = F.conv2d(x, window_1d, stride=1, padding=(0, padding), groups=C)
-    out = F.conv2d(out, window_1d.transpose(2, 3),
+    padding = 0 if not use_padding else window.size(3) // 2
+    out = F.conv2d(x, window, stride=1, padding=(0, padding), groups=C)
+    out = F.conv2d(out, window.transpose(2, 3),
                    stride=1, padding=(padding, 0), groups=C)
     return out
 
 
 @torch.jit.script
 def ssim(X, Y, window, data_range: float, use_padding: bool = False):
-    '''
-    Calculate ssim index for X and Y
-    :param X: images
-    :param Y: images
-    :param window: 1-D gauss kernel
-    :param data_range: value range of input images. (usually 1.0 or 255)
-    :param use_padding: padding image before conv
-    :return:
-    '''
-
+    """Calculate ssim for X and Y  
+    Args:
+        X (tensor):Y (tensor): a batch of images, (N, C, H, W)
+        window (tensor): 1-D gauss kernel
+        data_range (float): value range of input images. (usually 1.0 or 255)
+        use_padding (bool, optional): padding image before conv. Defaults to False.
+    """
     K1, K2 = 0.01, 0.03
     compensation = 1.0
 
     C1 = (K1 * data_range) ** 2
     C2 = (K2 * data_range) ** 2
 
-    mu1 = _gaussian_filter(X, window, use_padding)
-    mu2 = _gaussian_filter(Y, window, use_padding)
-    sigma1_sq = _gaussian_filter(X * X, window, use_padding)
-    sigma2_sq = _gaussian_filter(Y * Y, window, use_padding)
-    sigma12 = _gaussian_filter(X * Y, window, use_padding)
+    mu1 = gaussian_blur(X, window, use_padding)
+    mu2 = gaussian_blur(Y, window, use_padding)
+    sigma1_sq = gaussian_blur(X * X, window, use_padding)
+    sigma2_sq = gaussian_blur(Y * Y, window, use_padding)
+    sigma12 = gaussian_blur(X * Y, window, use_padding)
 
     mu1_sq = mu1.pow(2)
     mu2_sq = mu2.pow(2)
@@ -86,21 +80,18 @@ def ssim(X, Y, window, data_range: float, use_padding: bool = False):
 
 @torch.jit.script
 def ms_ssim(X, Y, window, data_range: float, weights, use_padding: bool = False):
-    '''
-    interface of ms-ssim
-    :param X: a batch of images, (N,C,H,W)
-    :param Y: a batch of images, (N,C,H,W)
-    :param window: 1-D gauss kernel
-    :param data_range: value range of input images. (usually 1.0 or 255)
-    :param weights: weights for different levels
-    :param use_padding: padding image before conv
-    :return:
-    '''
+    """Calculate ms_ssim for X and Y  
+    Args:
+        X (tensor):Y (tensor): a batch of images, (N, C, H, W)
+        window (tensor): 1-D gauss kernel
+        data_range (float): value range of input images. (usually 1.0 or 255)
+        weights (tensor): weights for different levels
+        use_padding (bool, optional): padding image before conv. Defaults to False.
+    """
     levels = weights.size(0)
     cs_vals, ssim_vals = [], []
     for _ in range(levels):
-        ssim_val, cs = ssim(X, Y, window=window,
-                            data_range=data_range, use_padding=use_padding)
+        ssim_val, cs = ssim(X, Y, window, data_range, use_padding)
         cs_vals.append(cs)
         ssim_vals.append(ssim_val)
         padding = (X.size(-2) % 2, X.size(-1) % 2)
@@ -113,18 +104,18 @@ def ms_ssim(X, Y, window, data_range: float, weights, use_padding: bool = False)
 
 
 class SSIM(torch.jit.ScriptModule):
+    """Structural Similarity index  
+    Args:
+        window_size (int, optional): the size of gauss kernel. Defaults to 11.
+        window_sigma (float, optional): sigma of normal distribution. Defaults to 1.5.
+        data_range (float, optional): value range of input images. (usually 1.0 or 255). Defaults to 255..
+        channel (int, optional): input channels. Defaults to 3.
+        use_padding (bool, optional): padding image before conv. Defaults to False.
+        reduction (str, optional): reduction mode. Defaults to "none".
+    """
     __constants__ = ['data_range', 'use_padding', 'reduction']
 
     def __init__(self, window_size=11, window_sigma=1.5, data_range=255., channel=3, use_padding=False, reduction="none"):
-        '''
-        :param window_size: the size of gauss kernel
-        :param window_sigma: sigma of normal distribution
-        :param scale: rerange to 255.
-        :param data_range: value range of input images. (usually 1.0 or 255)
-        :param channel: input channels (default: 3)
-        :param use_padding: padding image before conv
-        :param reduction: reduction mode
-        '''
         super().__init__()
         assert window_size % 2 == 1, 'Window size must be odd.'
         window = create_window(window_size, window_sigma, channel)
@@ -138,26 +129,26 @@ class SSIM(torch.jit.ScriptModule):
         ret = ssim(input, target, self.window,
                    self.data_range, self.use_padding)[0]
         if self.reduction != 'none':
-            ret = mean(ret) if self.reduction == 'mean' else sum(ret)
+            ret = ret.mean() if self.reduction == 'mean' else ret.sum()
         return ret
 
 
 class MS_SSIM(torch.jit.ScriptModule):
+    """Multi-Scale Structural Similarity index  
+    Args:
+        window_size (int, optional): the size of gauss kernel. Defaults to 11.
+        window_sigma (float, optional): sigma of normal distribution. Defaults to 1.5.
+        data_range (float, optional): value range of input images. (usually 1.0 or 255). Defaults to 255..
+        channel (int, optional): input channels. Defaults to 3.
+        use_padding (bool, optional): padding image before conv. Defaults to False.
+        reduction (str, optional): reduction mode. Defaults to "none".
+        weights (list of float, optional): weights for different levels. Default to [0.0448, 0.2856, 0.3001, 0.2363, 0.1333]
+        levels (int, optional): number of downsampling
+    """
     __constants__ = ['data_range', 'use_padding', 'reduction']
 
     def __init__(self, window_size=11, window_sigma=1.5, data_range=255., channel=3, use_padding=False, reduction="none",
                  weights=[0.0448, 0.2856, 0.3001, 0.2363, 0.1333], levels=None):
-        '''
-        :param window_size: the size of gauss kernel
-        :param window_sigma: sigma of normal distribution
-        :param scale: rerange to 255.
-        :param data_range: value range of input images. (usually 1.0 or 255)
-        :param channel: input channels
-        :param use_padding: padding image before conv
-        :param weights: weights for different levels. (default [0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
-        :param levels: number of downsampling
-        :param reduction: reduction mode
-        '''
         super().__init__()
         assert window_size % 2 == 1, 'Window size must be odd.'
         self.data_range = data_range
@@ -167,7 +158,7 @@ class MS_SSIM(torch.jit.ScriptModule):
         window = create_window(window_size, window_sigma, channel)
         self.register_buffer('window', window)
 
-        weights = torch.tensor(weights, dtype=torch.float)
+        weights = torch.Tensor(weights).float()
         if levels is not None:
             weights = weights[:levels]
             weights = weights / weights.sum()
@@ -179,5 +170,5 @@ class MS_SSIM(torch.jit.ScriptModule):
         ret = ms_ssim(input, target, self.window, self.data_range,
                       self.weights, self.use_padding)
         if self.reduction != 'none':
-            ret = mean(ret) if self.reduction == 'mean' else sum(ret)
+            ret = ret.mean() if self.reduction == 'mean' else ret.sum()
         return ret
